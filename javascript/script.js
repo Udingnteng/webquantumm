73ad3d5386e58de3f61570717c90c9220da72de3
QuantumCentral <165747132+Udingnteng@users.noreply.github.com>
2024-08-17 23:45:50 +0700

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -114,4 +114,12 @@ document.addEventListener("DOMContentLoaded", function() {
         notification.classList.remove('visible');
     });
 });
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+document.addEventListener('keydown', function(e) {
+    if (e.keyCode == 123 || (e.ctrlKey && e.shiftKey && (e.keyCode == 73))) {
+        e.preventDefault();
+    }
+});
 
